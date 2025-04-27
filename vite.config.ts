9507df774e4496c3bdfd3ff60b6973dcea280a57
AsMurphy-student <AsMurphy-work@protonmail.com>
2025-04-27 11:08:00 -0400

    Updated vite config to try to fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://asmurphy-student.github.io/nr2003-points-site/",
 })
