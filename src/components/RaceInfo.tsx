471b6170fac01fc9d62e5a16d9a0263e33a080e4
AsMurphy-student <AsMurphy-work@protonmail.com>
2025-04-28 22:11:32 -0400

    New build again

--- src/components/RaceInfo.tsx
+++ src/components/RaceInfo.tsx
@@ -6,8 +6,8 @@ function RaceInfo(props: {raceData: race, raceNumber: number}) {
   const { raceData, raceNumber } = props;
 
   return (
-    <Grid container spacing={2} direction={{sm: 'column', md: 'row'}} sx={{backgroundColor: 'black'}}>
-      <Grid minHeight='100%' sx={{maxWidth: {sm: '100%', md: '50%'}}}>
+    <Grid container spacing={2} direction={{sm: 'column', md: 'row'}} sx={{backgroundColor: 'black'}} width='100%'>
+      <Grid minHeight='100%' size='auto'>
         <h1 style={{color: '#fff'}}>#{raceNumber}: {raceData.raceName}</h1>
         <h2 style={{color: '#fff'}}>{raceData.raceDate}</h2>
         <h3 style={{color: '#fff'}}>Winner: {raceData.finishes[0].driverName}</h3>
@@ -17,7 +17,7 @@ function RaceInfo(props: {raceData: race, raceNumber: number}) {
         <h3 style={{color: '#fff'}}>{raceData.pitstopFrequency}</h3>
       </Grid>
 
-      <Grid minHeight='100%' sx={{maxWidth: {sm: '100%', md: '50%'}}}>
+      <Grid minHeight='100%' size='grow'>
         <h2 style={{color: '#fff', textAlign: 'center'}}>Laps Led</h2>
         <LapPieChart raceData={raceData}/>
       </Grid>
