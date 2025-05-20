b95146916ec2e6ded62b4dd68650cda5d459222a
AsMurphy-student <AsMurphy-work@protonmail.com>
2025-05-19 20:34:12 -0400

    Added starting position and average start to line graph on driver pages

--- src/components/DriverPage.tsx
+++ src/components/DriverPage.tsx
@@ -5,7 +5,6 @@ import { defaultTheme } from "../variables/defaultTheme";
 import { driver } from "../interfaces/driver";
 import NavBar from "./NavBar";
 import { LineChart, LineSeriesType } from "@mui/x-charts";
-import { useOrientation } from "react-use";
 import { useEffect, useState } from "react";
 // import { GridColDef } from "@mui/x-data-grid";
 
@@ -14,31 +13,47 @@ function DriverPage(props: { driverData: driver, raceIndex: number, driverIndex:
 
   const xAxisRaceArray = Array(racesData.length).fill(null).map((_,i) => i + 1);
 
-  const series: LineSeriesType = {
+  const startSeries: LineSeriesType = {
     curve: "linear",
-    data: driverData.finishPositions.map((finish) => {
-      if (finish > 0) {
-        return finish;
-      }
-      else {
-        return null;
-      }
+    data: driverData.startPositions.map((start) => {
+      return start > 0 ? start : null
     }),
-    label: 'Race Finish',
+    label: 'Starting Positions',
     type: 'line',
     showMark: true,
     valueFormatter: (value) => (value == null ? 'N/A' : value.toString()),  
   };
-  const arrayAvg = (driverData.finishPositions.reduce((a, b) => a + b) / driverData.finishes).toFixed(2);
-  const avgSeries: LineSeriesType = {
+  const startArrayAvg = (driverData.startPositions.reduce((a, b) => a + b) / driverData.starts).toFixed(2);
+  const avgStartSeries: LineSeriesType = {
     curve: "linear",
-    data: new Array(driverData.finishPositions.length).fill(arrayAvg),
-    label: 'Average',
+    data: new Array(driverData.startPositions.length).fill(startArrayAvg),
+    label: 'Starting Average',
     type: 'line',
     showMark: false,
     valueFormatter: (value) => (value == null ? '?' : value.toString()),  
   };
-  const lineSeries: LineSeriesType[] = [series, avgSeries];
+
+  const finishSeries: LineSeriesType = {
+    curve: "linear",
+    data: driverData.finishPositions.map((finish) => {
+      return finish > 0 ? finish : null  
+    }),
+    label: 'Finishing Positions',
+    type: 'line',
+    showMark: true,
+    valueFormatter: (value) => (value == null ? 'N/A' : value.toString()),  
+  };
+  const finishArrayAvg = (driverData.finishPositions.reduce((a, b) => a + b) / driverData.finishes).toFixed(2);
+  const avgFinishSeries: LineSeriesType = {
+    curve: "linear",
+    data: new Array(driverData.finishPositions.length).fill(finishArrayAvg),
+    label: 'Finishing Average',
+    type: 'line',
+    showMark: false,
+    valueFormatter: (value) => (value == null ? '?' : value.toString()),  
+  };
+
+  const lineSeries: LineSeriesType[] = [startSeries, avgStartSeries, finishSeries, avgFinishSeries];
 
   // const columns: GridColDef[] = [
   //     { 
@@ -50,11 +65,12 @@ function DriverPage(props: { driverData: driver, raceIndex: number, driverIndex:
   //     },
   // ];
 
-  const orientation = useOrientation();
   const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   useEffect(() => {
     const handleResize = () => {
       setWindowHeight(window.innerHeight);
+      setWindowWidth(window.innerWidth);
     };
 
     window.addEventListener('resize', handleResize);
@@ -70,7 +86,7 @@ function DriverPage(props: { driverData: driver, raceIndex: number, driverIndex:
       <Container maxWidth='xl' sx={{flexGrow: 1, minHeight: '100vh', minWidth: '100%', backgroundColor: 'secondary.main'}}>
         <h1 style={{ margin: 0, color: '#fff', paddingTop: '2rem'}}>{driverData.driverName}</h1>
         <Grid container spacing={0} direction='row'>
-          <Grid sx={{width: '100%'}} height={orientation.type === 'landscape-primary' ? 
+          <Grid sx={{width: '100%'}} height={windowWidth >= 900 ? 
                 windowHeight * 0.80
                 :
                 windowHeight * 0.40
@@ -79,10 +95,10 @@ function DriverPage(props: { driverData: driver, raceIndex: number, driverIndex:
               xAxis={[{ data: xAxisRaceArray, scaleType: 'point', label: 'Races' }]}
               yAxis={[{label: 'Finishes', reverse: true, min: 1, max: 43, tickMaxStep: 1}]}
               series={lineSeries}
-              height={orientation.type === 'landscape-primary' ? 
+              height={windowWidth >= 900 ? 
                 windowHeight * 0.80
                 :
-                windowHeight * 0.40
+                windowHeight * 0.60
               }
               hideLegend={false}
               grid={{ vertical: true, horizontal: true, }}
